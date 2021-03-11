c661eec8b224b875c1f6389af486b9b59fd484c5
pandhuwibowo <pandhuw58@gmail.com>
2021-03-12 00:59:30 +0700

    add user and login mockup

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/**
+ * Sign In
+ */
+Route::get('/signin', function () {
+    return view('front-layout.signin');
+});
 
-Route::get('/', function () {
-    return view('welcome');
+/**
+ * Users
+ */
+Route::prefix('users')->group(function() {
+    Route::get('/', function() {
+        return view('admin-dashboard.users.index');
+    });
 });
